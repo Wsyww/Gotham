d5f241a1759639f114205409f36df81d54dc9bf5
Shiyuan Wang <769188867@qq.com>
2022-04-10 17:18:37 +0800

    prettier

--- .prettier.js
+++ .prettier.js
@@ -14,4 +14,16 @@ module.exports = {
     },
   ],
   arrowParens: "always",
+  "husky": {
+    "hooks": {
+      "pre-commit": "lint-staged"
+    }
+  },
+  "lint-staged": {
+    "*.{js,jsx,vue,ts,tsx}": [
+      "yarn lint",
+      "prettier --write",
+      "git add"
+    ]
+  }
 };
